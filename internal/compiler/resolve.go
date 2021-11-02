98c1fe27e40e7bb55a4bbc62b3dcddab554f6aee
Timothy Studd <timstudd@gmail.com>
2021-11-02 07:20:02 -0700

    Feature: Add MySQL support for BETWEEN arguments

--- internal/compiler/resolve.go
+++ internal/compiler/resolve.go
@@ -19,6 +19,7 @@ func dataType(n *ast.TypeName) string {
 }
 
 func resolveCatalogRefs(c *catalog.Catalog, qc *QueryCatalog, rvs []*ast.RangeVar, args []paramRef, names map[int]string) ([]Parameter, error) {
+
 	aliasMap := map[string]*ast.TableName{}
 	// TODO: Deprecate defaultTable
 	var defaultTable *ast.TableName
@@ -193,6 +194,44 @@ func resolveCatalogRefs(c *catalog.Catalog, qc *QueryCatalog, rvs []*ast.RangeVa
 						Message:  fmt.Sprintf("column reference \"%s\" is ambiguous", key),
 						Location: left.Location,
 					}
+				}
+			}
+
+		case *ast.BetweenExpr:
+			if n == nil || n.Expr == nil || n.Left == nil || n.Right == nil {
+				fmt.Println("ast.BetweenExpr is nil")
+				continue
+			}
+
+			var key string
+			if ref, ok := n.Expr.(*ast.ColumnRef); ok {
+				if str, ok := ref.Fields.Items[0].(*ast.String); ok {
+					key = str.Str
+				}
+			}
+
+			number := 0
+			if pr, ok := n.Left.(*ast.ParamRef); ok {
+				number = pr.Number
+			}
+
+			for _, table := range tables {
+				schema := table.Schema
+				if schema == "" {
+					schema = c.DefaultSchema
+				}
+
+				if c, ok := typeMap[schema][table.Name][key]; ok {
+					a = append(a, Parameter{
+						Number: number,
+						Column: &Column{
+							Name:     parameterName(ref.ref.Number, key),
+							DataType: dataType(&c.Type),
+							NotNull:  c.IsNotNull,
+							IsArray:  c.IsArray,
+							Table:    table,
+						},
+					})
 				}
 			}
 
